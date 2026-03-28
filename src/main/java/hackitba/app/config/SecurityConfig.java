0652efbb9039fe2753aa0d4f509f012818658492
Elias Aires <eliasaires.2002@gmail.com>
2026-03-27 23:04:33 -0300

    configuration

--- src/main/java/hackitba/app/config/SecurityConfig.java
+++ src/main/java/hackitba/app/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package hackitba.app.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
     
 @Bean
