8e2f84e0965fc19c0740913c6184a769cc823dfa
Elias Aires <eliasaires.2002@gmail.com>
2026-03-28 02:45:50 -0300

    constructor vacio

--- src/main/java/hackitba/app/entitiy/MedicionCO2.java
+++ src/main/java/hackitba/app/entitiy/MedicionCO2.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class MedicionCO2 {
 
     @Id
